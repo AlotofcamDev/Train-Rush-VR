5be5850e6e4d65c75fd013e35fd3c01f0464d123
Michael-A-QUT <n11285303@qut.edu.au>
2024-05-08 22:44:00 +1000

    08/05/24.3
    - Fixed coal spawner making new coal every frame
    - Fixed coal spawner making coal in exact spot every times, making a pillar of coal
    - Increased roof height so you can't stand on your toes and peak out of the roof
    - Made entire furnace trigger for coal to be absorbed
    -Further layout work

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,8 +7,11 @@ public class Spawner : MonoBehaviour
 
     public Transform coalSpawner;
     public GameObject coal;
-   
 
+    public float coalSpawnRate;
+    private float coalTimer;
+
+    private Vector3 spawnPos;
 
     private void Update()
     {
@@ -17,12 +20,26 @@ public class Spawner : MonoBehaviour
             SpawnCoal();
             
         }
+
+        if (coalTimer < coalSpawnRate)
+        {
+            coalTimer += Time.deltaTime;
+        }
     }
 
 
     public void SpawnCoal()
     {
+        if (coalTimer >= coalSpawnRate)
+        {
+            spawnPos = coalSpawner.position + new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+            Instantiate(coal, spawnPos, Quaternion.identity);
+            coalTimer = 0;
+        }
+
+        /*
         Instantiate(coal, coalSpawner.position, Quaternion.identity);
         new WaitForSeconds(3f);
+        */
     }
 }
