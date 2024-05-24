7c5c7730bfedcb9c7a2dbf9dfca28a5fcfca18fc
Michael-A-QUT <n11285303@qut.edu.au>
2024-05-24 20:03:07 +1000

    24/05/24.4
    - Added audio source to ragdoll
    - Added audio clips to ragdoll audio source
    - Respective audio clips are played for each ragdoll function

--- Assets/Michael/RagdollScript.cs
+++ Assets/Michael/RagdollScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,16 +13,23 @@ public class RagdollScript : MonoBehaviour
     public OVRScreenFade screenFade;
     public ParticleSystem furnaceFire;
 
+    public AudioSource aSource;
+    public List<AudioClip> voicelines = new List<AudioClip>();
+
     // Start is called before the first frame update
     void Start()
     {
         if (isIntro)
         {
             Debug.Log("Hey! You there! You must be the new conductor! I'm the little guy on the shelf in front of you. Can you... pick me up real quick? While you're holding me, I'll show you around.");
+            aSource.Stop();
+            aSource.PlayOneShot(voicelines[7]);
         }
         else
         {
             Debug.Log("See? Totally fine. We're on our way! Chuck some coal in the furnace to keep us going!");
+            aSource.Stop();
+            aSource.PlayOneShot(voicelines[9]);
         }
     }
 
@@ -53,6 +61,8 @@ public class RagdollScript : MonoBehaviour
         {
             Debug.Log("What's up boss? I'll be your helper doll for this trip. Don't worry, I'll only bug you if we're in danger... or if I get bored.");
             Debug.Log("We should get moving, we've got passengers to move across. I know this'll be weird, but toss me in that furnace and I'll get this train started");
+            aSource.Stop();
+            aSource.PlayOneShot(voicelines[12]);
         }
     }
 
@@ -61,6 +71,9 @@ public class RagdollScript : MonoBehaviour
         if (isIntro)
         {
             Debug.Log("Ahhh! HOTHOTHOTHOT!!!!");
+            aSource.Stop();
+            aSource.PlayOneShot(voicelines[8]);
+
             screenFade.FadeOut();
             isFading = true;
             furnaceFire.Play();
@@ -73,17 +86,23 @@ public class RagdollScript : MonoBehaviour
         if (!isIntro)
         {
             Debug.Log("That's it boss! Nice and toasty!");
+            aSource.Stop();
+            aSource.PlayOneShot(voicelines[5]);
         }
     }
 
     public void FirstEnemy()
     {
         Debug.Log("Uhh... something's picking up on the train's sensors... I think we're being tailed by outlaws. Don't panic, we have those cannons outside for a reson. Check the port cannon and shoot them down!");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[4]);
     }
 
     public void DestroyFirstEnemy()
     {
         Debug.Log("You got them boss! And we got their bounty reward for our troubles! But I doubt they tried to derail us alone. Keep a lookout, and if you deal with enough of them, you can upgrade the train with the money you get.");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[3]);
     }
 
     // Game gameplay loop dialogue
@@ -91,39 +110,57 @@ public class RagdollScript : MonoBehaviour
     {
         Debug.Log("Enemies port side!");
         Debug.Log("Port side, boss!");
+        // Clip 14 or 15
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[Random.Range(14, 16)]);
     }
 
     public void EnemyStarboard()
     {
         Debug.Log("Enemies starboard side!");
         Debug.Log("Starboard side, boss!");
+        // Clip 23 or 24
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[Random.Range(23, 25)]);
     }
 
     public void DamagePort()
     {
         Debug.Log("Taking damage port side!");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[13]);
     }
 
     public void DamageStarboard()
     {
         Debug.Log("Taking damage starboard side!");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[22]);
     }
 
     public void CoalLow()
     {
         Debug.Log("Our coal's running low, boss!");
         Debug.Log("We're slowing down, add some coal!");
+        // Clip 0 or 1
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[Random.Range(0, 2)]);
     }
 
     public void HealthLow()
     {
         Debug.Log("We can't hold much longer!");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[2]);
     }
 
     public void GetUpgrade()
     {
         Debug.Log("Good idea, boss!");
         Debug.Log("This should help!");
+        // Clip 25 or 27
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[Random.Range(25, 27)]);
     }
 
     public void Toss()
@@ -132,12 +169,17 @@ public class RagdollScript : MonoBehaviour
         Debug.Log("AAHHHH");
         Debug.Log("AAAAAAAAAHHHH");
         Debug.Log("WEEEEEE");
-        Debug.Log("YAYYYY");
+        Debug.Log("YEE HAW");
+        // Clip 17, 18, 19, 20 or 21
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[Random.Range(17, 22)]);
     }
 
     public void EndGame()
     {
         Debug.Log("We made it to the station boss! Well done!");
+        aSource.Stop();
+        aSource.PlayOneShot(voicelines[6]);
         screenFade.FadeOut();
         isFading = true;
     }
