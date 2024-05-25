6209998162e19171f901593bd228b4af45fc3929
AlotofcamDev <101239015+AlotofcamDev@users.noreply.github.com>
2024-05-25 18:35:45 +1000

    FasterShooting Added
    - Added a Delay variable to the shooting to allow for customization of the speed
    - Feel free to revert if its broken

--- Assets/Corey/FireCannon.cs
+++ Assets/Corey/FireCannon.cs
@@ -6,28 +6,34 @@ public class FireCannon : MonoBehaviour
 {
     public float shotStrength;
     public float barrelOffset;
+    public float cooldownTime = 0.25f; // Cooldown time in seconds
 
     public DiegeticRotator left;
     public DiegeticRotator right;
     public GameObject bullet;
+    public Shop shop;
+
+
+    private bool canShoot = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(left.isGrabbed + ", " + right.isGrabbed);
-        if (left.isGrabbed && right.isGrabbed && (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)))
+        // Check if both controllers are grabbed and either index trigger is pressed
+        if (left.isGrabbed && right.isGrabbed && canShoot &&
+            (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)))
         {
             Shoot();
         }
 
-        // FOR DEBUGGING
-        if (Input.GetKeyDown(KeyCode.E))
+        // For debugging with the keyboard
+        if (Input.GetKeyDown(KeyCode.E) && canShoot)
         {
             Shoot();
         }
@@ -35,7 +41,25 @@ public class FireCannon : MonoBehaviour
 
     private void Shoot()
     {
+        // Instantiate the bullet at the specified position and apply force to it
         GameObject bulletInst = Instantiate(bullet, transform.forward * barrelOffset + transform.position, Quaternion.identity);
         bulletInst.GetComponent<Rigidbody>().AddForce(transform.forward * shotStrength, ForceMode.Impulse);
+
+        // Start the cooldown coroutine
+        
+        if(shop.FasterShooting == false)
+        {
+            StartCoroutine(Cooldown());
+        }
+        
+    }
+
+    private IEnumerator Cooldown()
+    {
+        // Set canShoot to false and wait for the cooldown time
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        // After the wait, set canShoot to true again
+        canShoot = true;
     }
 }
